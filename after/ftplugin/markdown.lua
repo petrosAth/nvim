9d5eb339747a3fbf8c3d8b759b43709eb3ef3c34
petrosAth <develop@athanasoulis.me>
2022-05-17 12:21:20 +0300

    Add wrap and textwidth for md filetype

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,5 +1,10 @@
 -- Show vertical line for text alignment
 vim.opt_local.colorcolumn = "80"
 
+-- Maximum width of text that is being inserted
+vim.opt_local.textwidth = 80
+-- When on, lines longer than the width of the window will wrap and displaying continues on the next line
+vim.opt_local.wrap = true
+
 -- When on spell checking will be done
 vim.opt_local.spell = true
