181959e3db94f3a764ff78a54839bfc38a7214d6
petrosAth <develop@athanasoulis.me>
2022-05-17 17:08:37 +0300

    Remove gg and G from neoscroll smoothing

--- lua/config/neoscroll.lua
+++ lua/config/neoscroll.lua
@@ -4,16 +4,16 @@ require("neoscroll").setup({
 
 local t = {}
 
-t["<C-u>"] = { "scroll", { "-vim.wo.scroll",                  "true",  "100" }}
-t["<C-d>"] = { "scroll", { "vim.wo.scroll",                   "true",  "100" }}
-t["<C-b>"] = { "scroll", { "-vim.api.nvim_win_get_height(0)", "true",  "100" }}
-t["<C-f>"] = { "scroll", { "vim.api.nvim_win_get_height(0)",  "true",  "100" }}
-t["<C-y>"] = { "scroll", { "-0.10",                           "false", "100" }}
-t["<C-e>"] = { "scroll", { "0.10",                            "false", "100" }}
-t["G"]     = { "scroll", { "vim.fn.line('$')",                "true",  "1"   }}
-t["gg"]    = { "gg",     {                                             "1"   }}
-t["zt"]    = { "zt",     {                                             "100" }}
-t["zz"]    = { "zz",     {                                             "100" }}
-t["zb"]    = { "zb",     {                                             "100" }}
+t["<C-y>"] = { "scroll", { "-0.10",                           "false", "80"  }}
+t["<C-e>"] = { "scroll", { "0.10",                            "false", "80"  }}
+t["<C-u>"] = { "scroll", { "-vim.wo.scroll",                  "true",  "150" }}
+t["<C-d>"] = { "scroll", { "vim.wo.scroll",                   "true",  "150" }}
+t["<C-b>"] = { "scroll", { "-vim.api.nvim_win_get_height(0)", "true",  "300" }}
+t["<C-f>"] = { "scroll", { "vim.api.nvim_win_get_height(0)",  "true",  "300" }}
+-- t["G"]     = { "scroll", { "vim.fn.line('$')",                "true",  "1"   }}
+-- t["gg"]    = { "gg",     {                                             "1"   }}
+t["zt"]    = { "zt",     {                                             "150" }}
+t["zz"]    = { "zz",     {                                             "150" }}
+t["zb"]    = { "zb",     {                                             "150" }}
 
 require("neoscroll.config").set_mappings(t)
