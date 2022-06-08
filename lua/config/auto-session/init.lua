2d187590bd25980dde50ed50b208306e37c80743
petrosAth <develop@athanasoulis.me>
2022-06-08 22:20:51 +0300

    Add trouble.nvim in auto-session auto close table

--- lua/config/auto-session/init.lua
+++ lua/config/auto-session/init.lua
@@ -1,9 +1,3 @@
--- local function restore_nvim_tree()
---     require("nvim-tree").change_dir(vim.fn.getcwd())
---     require("nvim-tree.lib").refresh_tree()
---     -- vim.cmd([[NvimTreeToggle]])
--- end
-
 require("auto-session").setup({
     auto_session_allowed_dirs = {
         "~/Develop/cs50g/",
@@ -15,6 +9,11 @@ require("auto-session").setup({
         "~/Develop/cs50g-project8-helicopter/",
         "~/.config/nvim/",
     },
-    pre_save_cmds = { "NvimTreeClose", "AerialClose", "UndotreeShow", "UndotreeHide" },
-    -- post_restore_cmds = { restore_nvim_tree },
+    pre_save_cmds = {
+        "NvimTreeClose",
+        "AerialClose",
+        "UndotreeShow",
+        "UndotreeHide",
+        "TroubleClose",
+    },
 })
