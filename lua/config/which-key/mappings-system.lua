2e2da95e838f94521a9cd8ab9dd96d97c58e7474
petrosAth <develop@athanasoulis.me>
2022-06-15 18:07:36 +0300

    Add terminal navigation and escape keybindings

--- lua/config/which-key/mappings-system.lua
+++ lua/config/which-key/mappings-system.lua
@@ -5,66 +5,78 @@ local n_opts = { noremap = true }
 -- "Undo chain" break points
 local break_points = { ".", ",", "!", "?", "=", "-", "_" }
 for _, v in pairs(break_points) do
-	map("i", tostring(v), v .. "<C-g>u", n_opts)
+    map("i", tostring(v), v .. "<C-g>u", n_opts)
+end
+
+local function replaceTermcodes(str)
+    -- Adjust boolean arguments as needed
+    return vim.api.nvim_replace_termcodes(str, true, true, true)
 end
 
 -- Normal mode mappings
 wk.register({
-    ["]b"]    = { "<CMD>bn<CR>",                  "Next buffer"                               },
-    ["[b"]    = { "<CMD>bp<CR>",                  "Previous buffer"                           },
-    ["]t"]    = { "<CMD>tabnext<CR>",             "Next tab"                                  },
-    ["[t"]    = { "<CMD>tabprevious<CR>",         "Previous tab"                              },
-    ["<F1>"]  = { "<CMD>setlocal spell!<CR>",     "Toggle spelling"                           },
-    ["<F2>"]  = { [[:%s/\<<C-r><C-w>\>/]],        "Replace word under cursor", silent = false },
-    ["<F3>"]  = { "<CMD>set relativenumber!<CR>", "Toggle relative number"                    },
-    ["<F11>"] = { "<CMD>set wrap!<CR>",           "Toggle wrap"                               },
-    ["<M-j>"] = { "v:count == 0 ? ':resize -5<CR>' : ':<C-u>resize -' . v:count1 . '<CR>'",                   "Contract horizontally", expr = true },
-    ["<M-k>"] = { "v:count == 0 ? ':resize +5<CR>' : ':<C-u>resize +' . v:count1 . '<CR>'",                   "Expand horizontally",   expr = true },
-    ["<M-h>"] = { "v:count == 0 ? ':vertical resize +5<CR>' : ':<C-u>vertical resize +' . v:count1 . '<CR>'", "Expand vertically",     expr = true },
-    ["<M-l>"] = { "v:count == 0 ? ':vertical resize -5<CR>' : ':<C-u>vertical resize -' . v:count1 . '<CR>'", "Contract vertically",   expr = true },
-    ["<M-J>"] = { ":m .+1<CR>==",                 "Move line up"                            },
-    ["<M-K>"] = { ":m .-2<CR>==",                 "Move line down"                          },
-    j         = { "v:count == 0 ? 'gj' : 'j'",    "Move using displayed lines", expr = true },
-    k         = { "v:count == 0 ? 'gk' : 'k'",    "Move using displayed lines", expr = true },
-    ["<Esc>"] = { ":noh<CR><Esc>",                "Clear search highlight",                 },
+    ["]b"]    = { "<CMD>bn<CR>",                                                                              "Next buffer",                                },
+    ["[b"]    = { "<CMD>bp<CR>",                                                                              "Previous buffer",                            },
+    ["]t"]    = { "<CMD>tabnext<CR>",                                                                         "Next tab",                                   },
+    ["[t"]    = { "<CMD>tabprevious<CR>",                                                                     "Previous tab",                               },
+    ["<F1>"]  = { "<CMD>setlocal spell!<CR>",                                                                 "Toggle spelling",                            },
+    ["<F2>"]  = { [[:%s/\<<C-r><C-w>\>/]],                                                                    "Replace word under cursor",  silent = false, },
+    ["<F3>"]  = { "<CMD>set relativenumber!<CR>",                                                             "Toggle relative number",                     },
+    ["<F11>"] = { "<CMD>set wrap!<CR>",                                                                       "Toggle wrap",                                },
+    ["<M-j>"] = { "v:count == 0 ? ':resize -5<CR>' : ':<C-u>resize -' . v:count1 . '<CR>'",                   "Contract horizontally",      expr   = true,  },
+    ["<M-k>"] = { "v:count == 0 ? ':resize +5<CR>' : ':<C-u>resize +' . v:count1 . '<CR>'",                   "Expand horizontally",        expr   = true,  },
+    ["<M-h>"] = { "v:count == 0 ? ':vertical resize +5<CR>' : ':<C-u>vertical resize +' . v:count1 . '<CR>'", "Expand vertically",          expr   = true,  },
+    ["<M-l>"] = { "v:count == 0 ? ':vertical resize -5<CR>' : ':<C-u>vertical resize -' . v:count1 . '<CR>'", "Contract vertically",        expr   = true,  },
+    ["<M-J>"] = { ":m .+1<CR>==",                                                                             "Move line up",                               },
+    ["<M-K>"] = { ":m .-2<CR>==",                                                                             "Move line down",                             },
+    j         = { "v:count == 0 ? 'gj' : 'j'",                                                                "Move using displayed lines", expr   = true,  },
+    k         = { "v:count == 0 ? 'gk' : 'k'",                                                                "Move using displayed lines", expr   = true,  },
+    ["<Esc>"] = { ":noh<CR><Esc>",                                                                            "Clear search highlight",                     },
     ["<Leader>"] = {
         d = {
             name = "Delete buffer/tab",
             b = { "<CMD>Bdelete<CR>",  "Delete buffer" },
-            t = { "<CMD>tabclose<CR>", "Delete tab"    }
+            t = { "<CMD>tabclose<CR>", "Delete tab"    },
         },
     },
 })
 
 -- Insert mode mappings
 wk.register({
-    ["<F1>"]  = { "<CMD>setlocal spell!<CR>",     "Toggle spelling"        },
-    ["<F3>"]  = { "<CMD>set relativenumber!<CR>", "Toggle relative number" },
+    ["<F1>"] = { "<CMD>setlocal spell!<CR>",     "Toggle spelling"        },
+    ["<F3>"] = { "<CMD>set relativenumber!<CR>", "Toggle relative number" },
 }, { mode = "i" })
 
 -- Visual and select mode mappings
 wk.register({
-    ["<F2>"]  = { [[y:%s/\V<C-r>"/]], "Replace word under cursor", silent = false },
-    ["<F3>"]  = { "<CMD>set relativenumber!<CR>", "Toggle relative number"        },
-    ["<M-J>"] = { ":m '>+1<CR>gv-gv", "Move line up"                              },
-    ["<M-K>"] = { ":m '<-2<CR>gv-gv", "Move line up"                              },
+    ["<F2>"]  = { [[y:%s/\V<C-r>"/]],             "Replace word under cursor", silent = false },
+    ["<F3>"]  = { "<CMD>set relativenumber!<CR>", "Toggle relative number"                    },
+    ["<M-J>"] = { ":m '>+1<CR>gv-gv",             "Move line up"                              },
+    ["<M-K>"] = { ":m '<-2<CR>gv-gv",             "Move line up"                              },
     ["<Space>"] = {
-        p     = { '"_dP', "Keep yanked text after paste" }
-    }
+        p = { '"_dP', "Keep yanked text after paste" },
+    },
 }, { mode = "v" })
 
 -- Operator mode mappings
-wk.register({
-}, { mode = "o" })
+wk.register({}, { mode = "o" })
 
 -- Visual mode mappings
-wk.register({
-}, { mode = "x" })
+wk.register({}, { mode = "x" })
 
 -- Terminal mode mappings
--- wk.register({
---     ["<esc>"] = { "<c-\\><c-n>", "Escape insert mode" }
--- }, { mode = "t" })
+wk.register({
+    ["<Esc>"] = {
+        ["<Esc>"]    = { "<Esc>",                          "Escape Neovim insert mode"   },
+        ["<Leader>"] = { replaceTermcodes([[<C-\><C-N>]]), "Escape terminal insert mode" },
+    },
+    ["<C-w>"] = {
+        h = { replaceTermcodes([[<C-\><C-N><C-w>h]]), "Go to the left window"  },
+        j = { replaceTermcodes([[<C-\><C-N><C-w>j]]), "Go to the down window"  },
+        k = { replaceTermcodes([[<C-\><C-N><C-w>k]]), "Go to the up window"    },
+        l = { replaceTermcodes([[<C-\><C-N><C-w>l]]), "Go to the right window" },
+    },
+}, { mode = "t" })
 
 -- Command mode mappings
 wk.register({
@@ -74,5 +86,5 @@ wk.register({
     ["<M-l>"] = { "<S-Right>", "Cursor one word right" },
 }, {
     mode = "c",
-    silent = false
+    silent = false,
 })
