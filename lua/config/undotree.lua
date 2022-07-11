f0f2f0e9f755531e67ad8125de05162972a521d0
petrosAth <develop@athanasoulis.me>
2022-07-11 21:08:47 +0300

    Increase undotree width and focus on open

--- lua/config/undotree.lua
+++ lua/config/undotree.lua
@@ -14,11 +14,9 @@ local g = vim.g
 
 g.undotree_WindowLayout       = 2
 g.undotree_ShortIndicators    = 1
-g.undotree_SplitWidth         = 40
+g.undotree_SplitWidth         = 30
 g.undotree_DiffpanelHeight    = 10
 g.undotree_DiffAutoOpen       = 0
 g.undotree_SetFocusWhenToggle = 1
--- g.undotree_TreeNodeShape      = "*"
--- g.undotree_TreeVertShape      = "│"
 g.undotree_HelpLine           = 0
 g.undotree_CursorLine         = 1
