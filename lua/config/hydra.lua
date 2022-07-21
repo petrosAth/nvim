4da7397d7e595089ca133cf6e8d98c94c8abe0b6
petrosAth <develop@athanasoulis.me>
2022-07-21 18:16:48 +0300

    Increase window size keybindings steps in hydra.nvim

--- lua/config/hydra.lua
+++ lua/config/hydra.lua
@@ -54,8 +54,8 @@ hydra({
 hydra({
     name = "Resize Window",
     hint = [[
-   _K_ / _k_  Increase height by 1 / 2      _H_ / _h_  Increase width by 1 / 3
-   _J_ / _j_  Decrease height by 1 / 2      _L_ / _l_  Decrease width by 1 / 3   ]],
+   _K_ / _k_  Increase height by 1 / 3      _H_ / _h_  Increase width by 1 / 5
+   _J_ / _j_  Decrease height by 1 / 3      _L_ / _l_  Decrease width by 1 / 5   ]],
     mode = { "n", "x" },
     body = "<M-w>",
     config = {
@@ -72,10 +72,10 @@ hydra({
         { "H", "<C-w><" },
         { "L", "<C-w>>" },
 
-        { "k", "<C-w>2+" },
-        { "j", "<C-w>2-" },
-        { "h", "<C-w>3<" },
-        { "l", "<C-w>3>" },
+        { "k", "<C-w>3+" },
+        { "j", "<C-w>3-" },
+        { "h", "<C-w>5<" },
+        { "l", "<C-w>5>" },
 
         { "q",     nil, { exit = true, nowait = true, desc = false } },
         { "<Esc>", nil, { exit = true, nowait = true, desc = false } },
