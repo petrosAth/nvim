1d11dd8bdac0196ce5d655606eedc5cd5a311d14
petrosAth <develop@athanasoulis.me>
2022-09-12 11:06:25 +0300

    Add packer preview in dashboard

--- lua/config/alpha.lua
+++ lua/config/alpha.lua
@@ -32,12 +32,12 @@ local header = {
 local buttons_navigation = {
     type = "group",
     val = {
-        set_button("LDR s r", i.lastSession[1] .. " " .. i.arrowr[1] .. " Load last session", "<CMD>PossessionLoad<CR>"),
-        set_button("SPC s s", i.sessions[1] .. " " .. i.arrowr[1] .. " Search sessions",   tele_custom .. "possession()<CR>"),
-        set_button("SPC s r", i.history[1] .. " " .. i.arrowr[1] .. " Recent files",      tele_custom .. "oldfiles()<CR>"),
-        set_button("SPC s R", i.history[1] .. " " .. i.arrowr[1] .. " Frecent files",     tele_custom .. "frecency()<CR>"),
-        set_button("SPC s f", i.search[1] .. " " .. i.arrowr[1] .. " File search",       "<CMD>Telescope find_files<CR>"),
-        set_button("SPC s g", i.grep[1] .. " " .. i.arrowr[1] .. " ripGREP search",    "<CMD>Telescope live_grep<CR>")
+        set_button("LDR s .", i.lastSession[1] .. " " .. i.arrowr[1] .. " Load last session", "<CMD>PossessionLoad<CR>"         ),
+        set_button("LDR s s", i.sessions[1]    .. " " .. i.arrowr[1] .. " Search sessions",   tele_custom .. "possession()<CR>" ),
+        set_button("SPC s r", i.history[1]     .. " " .. i.arrowr[1] .. " Recent files",      tele_custom .. "oldfiles()<CR>"   ),
+        set_button("SPC s R", i.history[1]     .. " " .. i.arrowr[1] .. " Frecent files",     tele_custom .. "frecency()<CR>"   ),
+        set_button("SPC s f", i.search[1]      .. " " .. i.arrowr[1] .. " File search",       "<CMD>Telescope find_files<CR>"   ),
+        set_button("SPC s g", i.grep[1]        .. " " .. i.arrowr[1] .. " ripGREP search",    "<CMD>Telescope live_grep<CR>"    )
     },
     opts = {
         spacing = 0,
@@ -47,8 +47,9 @@ local buttons_navigation = {
 local buttons_utility = {
     type = "group",
     val = {
-        set_button("LDR u u", i.update[1] .. " " .. i.arrowr[1] .. " Update plugins",      "<CMD>PackerSync<CR>"),
-        set_button("LDR u U", i.lspServers[1] .. " " .. i.arrowr[1] .. " Update LSP packages", "<CMD>Mason<CR><CMD>MasonToolsUpdate<CR>")
+        set_button("LDR u u p", i.diffview[1]   .. " " .. i.arrowr[1] .. " Preview plugins updates", "<CMD>PackerSync --preview<CR>"           ),
+        set_button("LDR u u P", i.update[1]     .. " " .. i.arrowr[1] .. " Update plugins",          "<CMD>PackerSync<CR>"                     ),
+        set_button("LDR u u l", i.lspServers[1] .. " " .. i.arrowr[1] .. " Update LSP packages",     "<CMD>Mason<CR><CMD>MasonToolsUpdate<CR>" ),
     },
     opts = {
         spacing = 0,
