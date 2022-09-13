881fc4366025e653ade8d65c70d99e2a078a7ef5
petrosAth <develop@athanasoulis.me>
2022-09-13 11:25:03 +0300

    Introduce new tabby file structure

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -393,10 +393,9 @@ return packer.startup(function()
     -- tabby.nvim - A minimal, configurable, neovim style tabline. Use your nvim tabs as workspace multiplexer.
     use({
         "nanozuki/tabby.nvim",
-        commit = "4d09e26",
         requires = "nvim-web-devicons",
         config = function()
-            require("config.tabby")
+            require("config.tabLine")
         end,
     })
 
