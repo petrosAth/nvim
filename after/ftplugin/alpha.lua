5e6352b4429636a120f8ed1003de4af8fd4a1514
petrosAth <develop@athanasoulis.me>
2022-09-14 13:12:52 +0300

    Enable tabline on dashboard

--- after/ftplugin/alpha.lua
+++ after/ftplugin/alpha.lua
@@ -2,5 +2,4 @@ local ol = vim.opt_local
 local f = require("options")
 
 ol.fillchars   = f.localFillchars
-ol.showtabline = 0                -- Disable tabline
 ol.winbar      = ""               -- Disable winbar
