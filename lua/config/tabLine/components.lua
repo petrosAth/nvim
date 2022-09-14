7fb4b3984325d0f4beade1970bee2290909a6787
petrosAth <develop@athanasoulis.me>
2022-09-14 13:16:28 +0300

    Create separator icon and hl function for tabby windows

--- lua/config/tabLine/components.lua
+++ lua/config/tabLine/components.lua
@@ -1,163 +1,107 @@
 local filename = require("tabby.module.filename")
-local text = require("tabby.text")
 local tab = require("tabby.tab")
 local i = require("styling").icons
 local s = require("styling").separators.default
-local also_use_thin_separators = true
+local use_thin_separators = true
+local p = require("config.themes.nord.palette") -- TODO: remove it after highlights are set
 
-local components = {}
-local c = components
+local M = {}
 
-c.modified_icon = i.edit[1]
-c.left_separator_icon = s[1]
-c.right_separator_icon = s[2]
-c.left_separator_icon_thin = s[3]
-c.right_separator_icon_thin = s[4]
-c.tab_close_icon = i.close[1]
+M.modified_icon = i.edit[1]
+M.left_separator_icon = s[1]
+M.right_separator_icon = s[2]
+M.left_separator_icon_thin = s[3]
+M.right_separator_icon_thin = s[4]
+M.tab_close_icon = i.close[1]
 
-local plugin_list = {
-    { filetype = "aerial",          window_title = i.codeOutline[1]  .. " Code outline"  },
-    { filetype = "alpha",           window_title = i.dashboard[1]    .. " Dashboard"     },
-    { filetype = "diff",            window_title = i.diffview[1]     .. " Diff Panel"    },
-    { filetype = "minimap",         window_title = i.minimap[1]      .. " Minimap"       },
-    { filetype = "neo-tree",        window_title = i.fileExplorer[1] .. " File explorer" },
-    { filetype = "NvimTree",        window_title = i.fileExplorer[1] .. " File explorer" },
-    { filetype = "Outline",         window_title = i.codeOutline[1]  .. " Code outline"  },
-    { filetype = "Trouble",         window_title = i.list[1]         .. " List"          },
-    { filetype = "TelescopePrompt", window_title = i.telescope[1]    .. " Telescope"     },
-    { filetype = "undotree",        window_title = i.undoTree[1]     .. " Undotree"      },
-}
-local filename_list = {
-    { filename = "%[Command Line%]",                     customFilename = i.history[1]    .. " History"                         },
-    { filename = "neo%-tree git_status",                 customFilename = i.git.repo[1]   .. " Git status"                      },
-    { filename = "neo%-tree buffers",                    customFilename = i.buffers[1]    .. " Open buffers"                    },
-    { filename = "/:0:/",                                customFilename =                    "Original file"                    },
-    { filename = "(/%.git/.+[a-z0-9]+[0-9]+[a-z0-9]+)/", customFilename = i.git.commit[1] .. " ",                gitRepo = true },
-    { filename = "^diffview:///panels/.*History",        customFilename = i.diffview[1]   .. " Diffview history"                },
-    { filename = "^diffview:///panels/.*",               customFilename = i.diffview[1]   .. " Diffview files"                  },
-    { filename = "^diffview:///.*",                      customFilename = i.diffview[1]   .. " Diffview"                        }
-}
-
-c.is_plugin = function(bufid)
-    local filetype = vim.api.nvim_buf_get_option(bufid, "filetype")
-    for _, plugin in pairs(plugin_list) do
-        if filetype == plugin.filetype then
-            return true, plugin.window_title
-        end
-    end
+local function is_last_win(line, index)
+    local win_count = #line.api.get_tab_wins(line.api.get_current_tab())
+    return index == win_count and true or false
 end
 
-c.has_custom_name = function(tabid, winid)
-    local winid = winid ~= "" and winid or vim.api.nvim_tabpage_get_win(tabid)
-    local fullPath = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(winid))
-    for _, name in pairs(filename_list) do
-        local filename_match = string.match(fullPath, name.filename)
-        if filename_match then
-            if name.gitRepo then
-                local commit = string.sub(filename_match, -11, -4)
-                return true, name.customFilename .. commit
+local function is_first_win(index)
+    return index == 1 and true or false
+end
+
+local theme = {
+    TabLine                        = { fg = p.nord4,     bg = p.nord1                     },
+    TabLineSel                     = { fg = p.nord4,     bg = p.nord3light                },
+    TabLineFill                    = { fg = p.nord4dark, bg = p.nord2                     },
+
+    TabLineWinSeparator            = { fg = p.nord4dark, bg = p.nord2                     },
+    TabLineWinSeparatorSel         = { fg = p.nord4dark, bg = p.nord3light                },
+    TabLineTabSeparator            = { fg = p.nord4,     bg = p.nord3                     },
+    TabLineTabSeparatorSel         = { fg = p.nord1,     bg = p.nord8                     },
+
+    TabLineSeparatorTail           = { fg = p.nord4,     bg = p.nord1                     },
+
+    TabLineTabIndicator            = { fg = p.nord4,     bg = p.nord3                     },
+    TabLineTabIndicatorSel         = { fg = p.nord1,     bg = p.nord8                     },
+    TabLineTabIndicatorModified    = { fg = p.nord13,    bg = p.nord3light                },
+    TabLineTabIndicatorModifiedSel = { fg = p.nord13,    bg = p.nord2                     },
+
+    TabLineHeader                  = { fg = p.nord1,     bg = p.nord8                     },
+    TabLineBody                    = { fg = p.nord4,     bg = p.nord1                     },
+    TabLineEdges                   = { fg = p.nord1,     bg = p.nord8,     style = "bold" },
+    TabLineTabCurrent              = { fg = p.nord1,     bg = p.nord8                     },
+    TabLineTabInactive             = { fg = p.nord4,     bg = p.nord3                     },
+    TabLineTabTopWinCurrent        = { fg = p.nord6,     bg = p.nord3light                },
+    TabLineTabTopWinInactive       = { fg = p.nord4dark, bg = p.nord2                     },
+    TabLineWinCurrent              = { fg = p.nord6,     bg = p.nord3light                },
+    TabLineWinInactive             = { fg = p.nord4,     bg = p.nord2                     },
+    TabLineWinModifiedCurrent      = { fg = p.nord13,    bg = p.nord3light                },
+    TabLineWinModifiedInactive     = { fg = p.nord13,    bg = p.nord2                     },
+    TabLineWinFillCurrent          = { fg = p.nord1,     bg = p.nord3light                },
+    TabLineWinFillInactive         = { fg = p.nord1,     bg = p.nord2                     },
+    test1 = { fg = "#bf616a", bg = "#ebcb8b" },
+    test2 = { fg = "#a3be8c", bg = "#b48ead" },
+    test3 = { fg = "#242a35", bg = "#8fbcbb" },
+    test4 = { fg = "#eceff4", bg = "#5e81ac" },
+}
+
+local function set_sep_icon(type, position, index)
+    local icon = ""
+    if type == "win" then
+        if position == "left" then
+            if is_first_win(index) then
+                icon = " "
+            else
+                icon = ""
             end
-            return true, name.customFilename
+        else
+            icon = ""
         end
     end
+    return icon
 end
 
-c.tab_label = function(tabid, current)
-    local number = vim.api.nvim_tabpage_get_number(tabid)
-    local name = tab.get_raw_name(tabid)
-    local label = string.format("%d", number)
-    if name ~= "" then
-        label = string.format("%s", name)
+local function set_sep_hl(line, type, position, is_current, index)
+    local fg = is_current and theme.TabLineSel or theme.TabLineFill
+    local bg = theme.TabLineFill
+    if type == "win" then
+        if position == "left" then
+            if is_first_win(index) then
+                fg = theme.TabLineTabSeparatorSel
+                bg = is_current and theme.TabLineSel or theme.TabLineFill
+            end
+        elseif position == "right" then
+            if is_last_win(line, index) then
+                bg = theme.TabLine
+            end
+        end
     end
-    if current then
-        return { label, hl = "TabLineTabCurrent" }
-    else
-        return { label, hl = "TabLineTabInactive" }
-    end
+    return fg, bg
 end
 
-c.tab_top_window = function(tabid, current)
-    local name = filename.unique(vim.api.nvim_tabpage_get_win(tabid))
-    local bufid = vim.api.nvim_win_get_buf(vim.api.nvim_tabpage_get_win(tabid))
-    local is_plugin, filetype = c.is_plugin(bufid)
-    local has_custom_name, custom_name = c.has_custom_name(tabid, "")
-    -- local label = string.format("%d : %s", bufid, name)
-    local label = string.format("%s", name)
-    if is_plugin then
-        label = string.format("%s", filetype)
-    end
-    if has_custom_name then
-        label = string.format("%s", custom_name)
-    end
-    if current then
-        return { label, hl = "TabLineTabTopWinCurrent" }
-    else
-        return { label, hl = "TabLineTabTopWinInactive" }
-    end
+-- @param line (var) heirline variable
+-- @param type (string: tab, win) tab or win
+-- @param position (string: left, right, between) separator position
+-- @param is_current (boolean) selected or not
+-- @param index (number) window position
+function M.set_sep_all(line, type, position, is_current, index)
+    local icon = set_sep_icon(type, position, index)
+    local fg, bg = set_sep_hl(line, type, position, is_current, index)
+    return icon, fg, bg
 end
 
-c.win_label = function(winid, current)
-    local bufid = vim.api.nvim_win_get_buf(winid)
-    local name = filename.unique(winid)
-    local is_plugin, filetype = c.is_plugin(bufid)
-    local has_custom_name, custom_name = c.has_custom_name("", winid)
-    -- local label = string.format("%d : %s", bufid, name)
-    local label = string.format("%s", name)
-    if is_plugin then
-        label = string.format("%s", filetype)
-    end
-    if has_custom_name then
-        label = string.format("%s", custom_name)
-    end
-    if current then
-        return { label, hl = "TabLineWinCurrent" }
-    else
-        return { label, hl = "TabLineWinInactive" }
-    end
-end
-
-c.modified_flag = function(current_window, modified)
-    local flag = { " " .. c.modified_icon, hl = "TabLineWinModifiedCurrent" }
-    if current_window then
-        flag.hl = modified and "TabLineWinModifiedCurrent" or "TabLineWinFillCurrent"
-    else
-        flag.hl = modified and "TabLineWinModifiedInactive" or "TabLineWinFillInactive"
-    end
-    return flag
-end
-
-c.separators = function(first_window, last_window, current_window)
-    local fg = "TabLineWinInactive"
-    local bg = "TabLineWinInactive"
-    local separator = {
-        left = text.separator(c.left_separator_icon, fg, bg),
-        right = text.separator(c.right_separator_icon, fg, bg),
-    }
-    -- If enabled, use thin separators between windows
-    if also_use_thin_separators then
-        fg = "TabLineBody"
-        separator.left = text.separator(c.left_separator_icon_thin, fg, bg)
-        separator.right = text.separator(c.right_separator_icon_thin, fg, bg)
-    end
-    -- Active window separators
-    if current_window then
-        fg = "TabLineWinCurrent"
-        separator.left = text.separator(c.left_separator_icon, fg, bg)
-        separator.right = text.separator(c.right_separator_icon, fg, bg)
-    end
-    -- Active tab right separator based on the backgrond of the first window(current/inactive)
-    if first_window then
-        fg = "TabLineTabCurrent"
-        bg = current_window and "TabLineWinCurrent" or "TabLineWinInactive"
-        separator.left = text.separator(c.right_separator_icon .. " ", fg, bg)
-    end
-    -- Last window separator
-    if last_window then
-        fg = current_window and "TabLineWinCurrent" or "TabLineWinInactive"
-        bg = "TabLineBody"
-        separator.right = text.separator(c.right_separator_icon, fg, bg)
-    end
-    return separator
-end
-
-return components
+return M
--- lua/config/tabLine/init.lua
+++ lua/config/tabLine/init.lua
@@ -1,49 +1,93 @@
 local tabline = require("tabby.tabline")
--- local c = require("config.tabLine.components")
+local c = require("config.tabLine.components")
+local i = require("styling").icons
+local s = require("styling").separators.default
+local p = require("config.themes.nord.palette") -- TODO: remove it after highlights are set
 
 local theme = {
-  fill = 'TabLineFill',
-  -- Also you can do this: fill = { fg='#f2e9de', bg='#907aa9', style='italic' }
-  head = 'TabLine',
-  current_tab = 'TabLineSel',
-  tab = 'TabLine',
-  win = 'TabLine',
-  tail = 'TabLine',
+    TabLine                        = { fg = p.nord4,     bg = p.nord1                     },
+    TabLineSel                     = { fg = p.nord4,     bg = p.nord3light                },
+    TabLineFill                    = { fg = p.nord4dark, bg = p.nord2                     },
+
+    TabLineWinSeparator            = { fg = p.nord4dark, bg = p.nord2                     },
+    TabLineWinSeparatorSel         = { fg = p.nord4dark, bg = p.nord3light                },
+    TabLineTabSeparator            = { fg = p.nord4,     bg = p.nord3                     },
+    TabLineTabSeparatorSel         = { fg = p.nord1,     bg = p.nord8                     },
+
+    TabLineSeparatorTail           = { fg = p.nord4,     bg = p.nord1                     },
+
+    TabLineTabIndicator            = { fg = p.nord4,     bg = p.nord3                     },
+    TabLineTabIndicatorSel         = { fg = p.nord1,     bg = p.nord8                     },
+    TabLineTabIndicatorModified    = { fg = p.nord13,    bg = p.nord3light                },
+    TabLineTabIndicatorModifiedSel = { fg = p.nord13,    bg = p.nord2                     },
+
+    TabLineHeader                  = { fg = p.nord1,     bg = p.nord8                     },
+    TabLineBody                    = { fg = p.nord4,     bg = p.nord1                     },
+    TabLineEdges                   = { fg = p.nord1,     bg = p.nord8,     style = "bold" },
+    TabLineTabCurrent              = { fg = p.nord1,     bg = p.nord8                     },
+    TabLineTabInactive             = { fg = p.nord4,     bg = p.nord3                     },
+    TabLineTabTopWinCurrent        = { fg = p.nord6,     bg = p.nord3light                },
+    TabLineTabTopWinInactive       = { fg = p.nord4dark, bg = p.nord2                     },
+    TabLineWinCurrent              = { fg = p.nord6,     bg = p.nord3light                },
+    TabLineWinInactive             = { fg = p.nord4,     bg = p.nord2                     },
+    TabLineWinModifiedCurrent      = { fg = p.nord13,    bg = p.nord3light                },
+    TabLineWinModifiedInactive     = { fg = p.nord13,    bg = p.nord2                     },
+    TabLineWinFillCurrent          = { fg = p.nord1,     bg = p.nord3light                },
+    TabLineWinFillInactive         = { fg = p.nord1,     bg = p.nord2                     },
 }
+
 tabline.set(function(line)
-  return {
-    {
-      { '  ', hl = theme.head },
-      line.sep('', theme.head, theme.fill),
-    },
-    line.wins_in_tab(line.api.get_current_tab()).foreach(function(win)
-      return {
-        line.sep('', theme.win, theme.fill),
-        win.is_current() and '' or '',
-        win.buf_name(),
-        line.sep('', theme.win, theme.fill),
-        hl = theme.win,
-        margin = ' ',
-      }
-    end),
-    line.spacer(),
-    line.tabs().foreach(function(tab)
-      local hl = tab.is_current() and theme.current_tab or theme.tab
-      return {
-        line.sep('', hl, theme.fill),
-        tab.is_current() and '' or '',
-        tab.number(),
-        tab.name(),
-        tab.close_btn(''),
-        line.sep('', hl, theme.fill),
-        hl = hl,
-        margin = ' ',
-      }
-    end),
-    {
-      line.sep('', theme.tail, theme.fill),
-      { '  ', hl = theme.tail },
-    },
-    hl = theme.fill,
-  }
+    local win_index = 0
+    return {
+        {
+            { "  ", hl = theme.TabLineHeader },
+            line.sep("", theme.TabLineHeader, theme.TabLineSeparatorTail),
+        },
+        {
+            line.sep("", theme.TabLineTabSeparatorSel, theme.TabLineSeparatorTail),
+            line.api.get_tab_number(line.api.get_current_tab()),
+            hl = theme.TabLineTabIndicatorSel,
+            margin = "",
+        },
+        line.wins_in_tab(line.api.get_current_tab()).foreach(function(win)
+            win_index = win_index + 1
+            local hl = win.is_current() and theme.TabLineSel or theme.TabLineFill
+            return {
+                line.sep(c.set_sep_all(line, "win", "left", win.is_current(), win_index)),
+                win.buf_name(),
+                { " " },
+                { "" },
+                line.sep(c.set_sep_all(line, "win", "right", win.is_current(), win_index)),
+                hl = hl,
+                margin = "",
+            }
+        end),
+        line.spacer(),
+        line.tabs().foreach(function(tab)
+            local hl = tab.is_current() and theme.TabLineSel or theme.TabLineFill
+            local hl_s = tab.is_current() and theme.TabLineTabSeparatorSel or theme.TabLineTabSeparator
+            local hl_s_internal = tab.is_current() and theme.TabLineSel or theme.TabLineFill
+            local hl_i = tab.is_current() and theme.TabLineTabIndicatorSel or theme.TabLineTabIndicator
+            return {
+                line.sep("", hl_s, theme.TabLine),
+                { " ", hl = hl_i },
+                { tab.number(), hl = hl_i },
+                { " ", hl = hl_i },
+                line.sep("", hl_s, hl_s_internal),
+                { " " },
+                tab.name(),
+                { " " },
+                tab.close_btn(""),
+                { " " },
+                line.sep(" ", hl, theme.TabLineSeparatorTail),
+                hl = hl,
+                margin = "",
+            }
+        end),
+        {
+            line.sep("", theme.TabLineHeader, theme.TabLineSeparatorTail),
+            { "  ", hl = theme.TabLineHeader },
+        },
+        hl = theme.fill,
+    }
 end)
