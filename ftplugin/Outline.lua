039cf0d26a18cb16f332f244abb80fa9436acb9e
petrosAth <develop@athanasoulis.me>
2022-10-09 18:37:01 +0300

    Clean up after aerial/symbols-outline swap

--- ftplugin/Outline.lua
+++ ftplugin/Outline.lua
@@ -1,6 +1,7 @@
 local ol = vim.opt_local
 local i = require("styling").icons.fillchars
 
+ol.colorcolumn    = ""        -- Hide vertical line for text alignment
 ol.number         = true
 ol.relativenumber = true
 ol.foldcolumn     = "0"       -- Fold column size
--- ftplugin/aerial.lua
+++
@@ -1,7 +0,0 @@
-local ol = vim.opt_local
-local i = require("styling").icons.fillchars
-
-ol.number         = true
-ol.relativenumber = true
-ol.fillchars      = i.global  -- Re-apply fillchars because aerial resets them
-ol.fillchars:append(i.custom) -- Remove eob character
