4f4c12aee09b6d86fbb0026f665a078532da9b7f
petrosAth <develop@athanasoulis.me>
2022-10-15 16:41:28 +0300

    Map <C-h> and <C-w> in nvim-autopairs

--- lua/config/autopairs/init.lua
+++ lua/config/autopairs/init.lua
@@ -2,11 +2,13 @@ local npairs = require("nvim-autopairs")
 
 local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 local cmp = require("cmp")
-cmp.event:on( "confirm_done", cmp_autopairs.on_confirm_done({  map_char = { tex = "" } }))
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 
-npairs.setup{
-    disable_filetype = { "TelescopePrompt", },
-    map_c_h = true
-}
+npairs.setup({
+    disable_filetype = { "TelescopePrompt" },
+    map_cr = true,
+    map_c_h = true,
+    map_c_w = true,
+})
 
 require("config.autopairs.rules").add_rules()
