51763e41a611cab1aabe77dc3063374c7b86119d
petrosAth <develop@athanasoulis.me>
2022-10-16 23:44:54 +0300

    Add .gitignore file in the project config function

--- lua/projects.lua
+++ lua/projects.lua
@@ -1,16 +1,51 @@
----Create a new file and open it in a new buffer in a vertical split
+---Creates a file with the given content on the specified path.
+---@param path string The path of the file
+---@param file string The name of the file
+---@param content table Text content in the form of a table with one line of
+---text per entry.
+local function create_file(path, file, content)
+    if vim.fn.isdirectory(path) == 0 then
+        vim.fn.mkdir(path)
+    end
+
+    for _, line in ipairs(content) do
+        os.execute("echo '" .. line .. "' >> " .. path .. "/" .. file)
+    end
+end
+
+---Open a new buffer in the current window with the given name, pointing at the
+---given path. If content is given, populate it with it. Also create the needed
+---directories if they don't exist.
 ---@param dir string Parent directory of the file.
 ---@param file string Name of the file.
----@param content table Text content in the form of a table with one line of text per entry.
+---@param content table Text content in the form of a table with one line of
+---text per entry.
 ---@param cursor table Starting cursor position.
 local function create_buffer(dir, file, content, cursor)
     content = content or {}
     cursor = cursor or {}
-    vim.cmd.vnew(dir .. "/" .. file)
+
+    if vim.fn.isdirectory(dir) == 0 then
+        vim.fn.mkdir(dir)
+    end
+
+    vim.cmd.edit(dir .. "/" .. file)
     local buf_id = vim.api.nvim_win_get_buf(0)
 
     vim.api.nvim_buf_set_lines(buf_id, 0, -1, false, content)
     vim.api.nvim_win_set_cursor(0, cursor)
+end
+
+---Create and populate .gitignore file
+---@param cwd string? Current working directory.
+local function create_gitignore(cwd)
+    cwd = cwd or vim.fn.getcwd()
+    local file = ".gitignore"
+    local content = {
+        [[/.nvim/spell/]],
+    }
+
+    create_file(cwd, file, content)
 end
 
 ---Create a palette template in the local project's configuration directory.
@@ -24,22 +59,17 @@ function PA.create_local_palette(config_dir)
     local content = {
         [[{]],
         [[  "colour_table": {]],
-        [[    "color1": "#ffffff",]],
-        [[    "color2": "#000000"]],
+        [[    "color": "#ffffff"]],
         [[  }]],
         [[}]],
     }
 
-    if vim.fn.isdirectory(path) == 0 then
-        vim.fn.mkdir(path)
-    end
-
     create_buffer(path, file, content, { 3, 5 })
 end
 
----Load custom hexokinase palettes from the project's local configuration directory.
----If the current working directory is the Neovim config path, load the
----currently active theme palette as well.
+---Load custom hexokinase palettes from the project's local configuration
+---directory. If the current working directory is the Neovim config path, load
+---the currently active theme palette as well.
 ---@param cwd string Current working directory.
 ---@param config_dir string Local project's configuration directory.
 ---@return table palettes Table containing the palettes.
@@ -86,9 +116,6 @@ end
 ---@param config table Table containing the loading options.
 function PA.load_local_config(cwd, config)
     local dir = PA.local_config.dir
-    -- if string.find(cwd, "%" .. dir) ~= nil then
-    --     return
-    -- end
 
     if config.use_session == true then
         PA.save_local_session(cwd)
@@ -101,10 +128,10 @@ function PA.load_local_config(cwd, config)
     end
 end
 
----Create a project local config file, and open it in the editor.
+---Create a project local config file, and open it in the current window.
 function PA.create_local_config()
     local dir = PA.local_config.dir
-    local file = "init.local.lua"
+    local file = PA.local_config.file
     local content = {
         [[PA.load_local_config(vim.fn.getcwd(), {]],
         [[    use_session   = false,]],
@@ -113,7 +140,26 @@ function PA.create_local_config()
         [[})]],
     }
 
+    create_gitignore()
     vim.fn.mkdir(dir)
     create_buffer(dir, file, content, { 2, 20 })
     vim.cmd.redraw()
+end
+
+---Edit the local config file in the current window.
+---@param cwd string? Current working directory.
+function PA.edit_local_config(cwd)
+    cwd = cwd or vim.fn.getcwd()
+    local dir = PA.local_config.dir
+    local file = PA.local_config.file
+
+    vim.cmd.edit(dir .. "/" .. file)
+end
+
+---Edit the .gitignore file in the current window.
+---@param cwd string? Current working directory.
+function PA.edit_gitignore(cwd)
+    cwd = cwd or vim.fn.getcwd()
+
+    vim.cmd.edit(cwd .. "/" .. ".gitignore")
 end
