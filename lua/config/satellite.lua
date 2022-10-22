e90cd792ef8506d3088b6d2ac1fcffaf142a5b26
petrosAth <develop@athanasoulis.me>
2022-10-22 19:36:52 +0300

    Change codewindow z_index

--- lua/config/satellite.lua
+++ lua/config/satellite.lua
@@ -1,22 +1,8 @@
 require("satellite").setup({
-    current_only = false,
     winblend = PA.styling.variables.transparency,
-    zindex = 40,
-    excluded_filetypes = {},
-    width = 0,
     handlers = {
-        search = {
-            enGble = true,
-        },
-        diagnostic = {
-            enable = true,
-        },
-        gitsigns = {
-            enable = true,
-        },
         marks = {
             enable = false,
-            show_builtins = false, -- shows the builtin marks like [ ] < >
         },
     },
 })
