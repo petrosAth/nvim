f13c5fecbf11785b567a71737d2e80025d6ce45a
petrosAth <develop@athanasoulis.me>
2022-11-16 10:54:48 +0200

    Change todo-comments icons to codicons

--- lua/config/todo-comments.lua
+++ lua/config/todo-comments.lua
@@ -1,19 +1,21 @@
+local i = PA.styling.icons
+
 require("todo-comments").setup({
     signs = true, -- show icons in the signs column
     sign_priority = 8, -- sign priority
     -- keywords recognized as todo comments
     keywords = {
         FIX = {
-            icon = "", -- icon used for the sign, and in search results
+            icon = i.bug[1], -- icon used for the sign, and in search results
             color = "error", -- can be a hex color, or a named color (see below)
             alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
             -- signs = false, -- configure signs for some keywords individually
         },
-        TODO = { icon = "", color = "info" },
-        HACK = { icon = "", color = "warning" },
-        WARN = { icon = "", color = "warning", alt = { "WARNING", "XXX" } },
-        PERF = { icon = "", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
-        NOTE = { icon = "", color = "hint", alt = { "INFO" } },
+        TODO = { icon = i.todo[1], color = "info" },
+        HACK = { icon = i.hack[1], color = "warning" },
+        WARN = { icon = i.lsp.warn[1], color = "warning", alt = { "WARNING", "XXX" } },
+        PERF = { icon = i.performance[1], alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+        NOTE = { icon = i.note[1], color = "hint", alt = { "INFO" } },
     },
     merge_keywords = true, -- when true, custom keywords will be merged with the defaults
     -- highlighting of the line containing the todo comment
