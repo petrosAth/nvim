ca17b9a0ef59d56d4fd41599d6cc0d14c9544fc5
petrosAth <develop@athanasoulis.me>
2022-11-20 12:46:15 +0200

    Show neovim nighly changelog on alpha

--- after/ftplugin/alpha.lua
+++ after/ftplugin/alpha.lua
@@ -5,3 +5,10 @@ local i = USER.styling.icons.fillchars
 ol.winbar    = nil            -- Disable winbar
 ol.fillchars = i.global       -- Re-apply fillchars because Trouble resets them
 ol.fillchars:append(i.custom) -- Remove eob character
+
+-- Show neovim nightly changelog on the dashboard if the current version is a prerelease
+if vim.version()["prerelease"] then
+    vim.cmd("vert help news")
+    vim.cmd("101wincmd|")
+    vim.cmd("wincmd p")
+end
--- after/ftplugin/undotree.lua
+++ after/ftplugin/undotree.lua
@@ -2,6 +2,6 @@
 local ol = vim.opt_local
 local i = USER.styling.icons.fillchars
 
-ol.colorcolumn = ""           -- Hide vertical line for text alignment
-ol.fillchars      = i.global  -- Re-apply fillchars because undotree resets them
+ol.colorcolumn = ""       -- Hide vertical line for text alignment
+ol.fillchars   = i.global -- Re-apply fillchars because undotree resets them
 ol.fillchars:append(i.custom) -- Remove eob character
