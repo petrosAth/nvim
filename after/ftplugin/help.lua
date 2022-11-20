45db06e71de601d52b9bfeb83ea623dec1de2230
petrosAth <develop@athanasoulis.me>
2022-11-20 11:42:25 +0200

    Change defaults for man and help filetypes

--- after/ftplugin/help.lua
+++ after/ftplugin/help.lua
@@ -1,4 +1,4 @@
 local ol = vim.opt_local
 
-ol.conceallevel = 0 -- Keep all characters visible
-ol.colorcolumn = "" -- Hide vertical line for text alignment
+ol.colorcolumn = ""   -- Hide vertical line for text alignment
+ol.signcolumn  = "no" -- Hide sign column
--- after/ftplugin/man.lua
+++ after/ftplugin/man.lua
@@ -1,5 +1,6 @@
 local ol = vim.opt_local
 
+ol.colorcolumn    = ""    -- Hide vertical line for text alignment
 ol.number         = false -- Hide line numbers
 ol.relativenumber = false -- Disable relative line numbers
 ol.signcolumn     = "no"  -- Hide sign column
