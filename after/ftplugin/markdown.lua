d5ad72f8b62f4940e9f3b48b3112be2994a75a87
petrosAth <develop@athanasoulis.me>
2022-11-23 13:43:20 +0200

    Enable spelling in markdown files
    Re-enable spelling in markdown files

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -3,3 +3,4 @@ local ol = vim.opt_local
 ol.colorcolumn = "80" -- Vertical line for text alignment position
 ol.textwidth   = 80   -- Maximum width of text that is being inserted
 ol.wrap        = true -- Wrap lines longer than the window width
+ol.spell       = true -- When on spell checking will be done
