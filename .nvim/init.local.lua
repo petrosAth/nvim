924142ab4002b8abed8828fe7d16b31d5b1fba64
petrosAth <develop@athanasoulis.me>
2022-11-24 02:18:30 +0200

    Update init.local.lua

--- .nvim/init.local.lua
+++ .nvim/init.local.lua
@@ -1,6 +1,7 @@
-USER.load_local_config(vim.fn.getcwd(), {
-    use_session        = true,
-    use_spellfile      = true,
-    use_palettes       = true,
-    use_format_on_save = false,
+USER.load_local_config({
+    use_session        = true,  -- Use local session
+    use_spellfile      = true,  -- Use local spell file
+    use_vale           = true,  -- Use Vale linter(with local spell file)
+    use_palettes       = true,  -- Use local hexokinase palettes
+    use_format_on_save = false, -- Enable LSP format on save
 })
