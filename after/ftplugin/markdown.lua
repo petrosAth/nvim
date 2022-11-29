a70d3f410c5ef64951d39f836cab8d69d0ea2206
petrosAth <develop@athanasoulis.me>
2022-11-29 09:15:36 +0200

    Change markdown ftplugin

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -4,3 +4,6 @@ ol.colorcolumn = "80" -- Vertical line for text alignment position
 ol.textwidth   = 80   -- Maximum width of text that is being inserted
 ol.wrap        = true -- Wrap lines longer than the window width
 ol.spell       = true -- When on spell checking will be done
+ol.shiftwidth  = 2    -- Number of auto-indent spaces
+ol.softtabstop = 2    -- Number of spaces per Tab
+ol.tabstop     = 2    -- Number of columns per tab
