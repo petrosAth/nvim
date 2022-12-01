c1526f1b24df7031bbafef8b86f4ce37a6b228ba
petrosAth <develop@athanasoulis.me>
2022-12-01 14:55:57 +0200

    Use the same charactre set for indent_blankline current context

--- lua/config/indent-blankline-config.lua
+++ lua/config/indent-blankline-config.lua
@@ -26,7 +26,7 @@ local excBuftypes = {
 
 require("indent_blankline").setup({
     char_list = { i.char[1], i.char[2] },
-    context_char_list = { i.context_char[1], i.context_char[2] },
+    context_char_list = { i.context_char[1] },
     space_char_blankline = " ",
     filetype_exclude = excFiletypes,
     buftype_exclude = excBuftypes,
@@ -35,7 +35,7 @@ require("indent_blankline").setup({
     show_end_of_line = false,
     show_current_context = true, -- Need treesitter
     show_current_context_start = true, -- Need treesitter
-    show_current_context_start_on_current_line = false,
+    show_current_context_start_on_current_line = true, -- Need treesitter
     use_treesitter = true, -- Need treesitter
     indent_level = 20,
     show_foldtext = true,
