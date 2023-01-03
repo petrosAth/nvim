c331712c387fcff5270dd0d518d20276063d7be9
petrosAth <develop@athanasoulis.me>
2023-01-03 16:21:43 +0200

    Use LspAttach autocmd for lsp-signature

--- lua/config/lsp/lsp-signature-config.lua
+++ lua/config/lsp/lsp-signature-config.lua
@@ -1,9 +1,13 @@
-local lsp_signature = require("lsp_signature")
+local loaded, lsp_signature = pcall(require, "lsp_signature")
+if not loaded then
+    return
+end
+
 local t = USER.styling.variables.transparency
 local i = USER.styling.icons
 local b = USER.styling.borders.default
 
-lsp_signature.setup({
+local config = {
     floating_window_above_cur_line = true, -- try to place the floating above the current line when possible Note:
     hint_enable = false, -- virtual hint enable
     hint_prefix = i.lsp.hint[1] .. " ",
@@ -15,4 +19,11 @@ lsp_signature.setup({
     toggle_key = "<M-s>", -- toggle signature on and off in insert mode,  e.g. toggle_key = '<M-x>'
 
     select_signature_key = "<M-S>", -- cycle to next signature, e.g. '<M-n>' function overloading
+}
+
+vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(args)
+        local bufnr = args.buf
+        lsp_signature.on_attach(config, bufnr)
+    end,
 })
