ed694001216de10d72d7ddd954fd12c23b404719
petrosAth <develop@athanasoulis.me>
2023-01-05 21:27:41 +0200

    Disable cursor animation and reduce timings

--- lua/config/mini-config.lua
+++ lua/config/mini-config.lua
@@ -10,36 +10,24 @@ function M.setup()
     animate.setup({
         -- Cursor path
         cursor = {
-            -- Whether to enable this animation
-            enable = true,
-
-            -- Timing of animation (how steps will progress in time)
-            timing = animate.gen_timing.linear({ duration = 150, unit = "total" }),
+            enable = false,
         },
         -- Vertical scroll
         scroll = {
-            -- Whether to enable this animation
             enable = true,
-
-            -- Timing of animation (how steps will progress in time)
             timing = animate.gen_timing.linear({ duration = 150, unit = "total" }),
         },
         -- Window resize
         resize = {
-            -- Whether to enable this animation
             enable = true,
-
-            -- Timing of animation (how steps will progress in time)
             timing = animate.gen_timing.linear({ duration = 150, unit = "total" }),
         },
         -- Window open
         open = {
-            -- Whether to enable this animation
             enable = false,
         },
         -- Window close
         close = {
-            -- Whether to enable this animation
             enable = false,
         },
     })
