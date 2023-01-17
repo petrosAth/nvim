d1a55fbc6e0801c97a8263a47bd927a104631edd
petrosAth <develop@athanasoulis.me>
2023-01-17 22:25:07 +0200

    Disable neoscroll.nvim

--- lua/plugins/neoscroll.lua
+++ lua/plugins/neoscroll.lua
@@ -1,15 +1,15 @@
 local t = {
-    ["<C-y>"] = { "scroll", { "-0.10",                             "true",  "80"   } },
-    ["<C-e>"] = { "scroll", { "0.10",                              "true",  "80"   } },
-    ["<C-u>"] = { "scroll", { "-vim.wo.scroll",                    "false", "150"  } },
-    ["<C-d>"] = { "scroll", { "vim.wo.scroll",                     "false", "150"  } },
-    ["<C-b>"] = { "scroll", { "-vim.api.nvim_win_get_height(0)",   "true",  "300"  } },
-    ["<C-f>"] = { "scroll", { "vim.api.nvim_win_get_height(0)",    "true",  "300"  } },
-    -- ["G"]     = { "scroll", { "2*vim.api.nvim_buf_line_count(0)",  "true",  "1000" } },
-    -- ["gg"]    = { "scroll", { "-2*vim.api.nvim_buf_line_count(0)", "true",  "1000" } },
-    ["zt"]    = { "zt",     { "150"                                }                 },
-    ["zz"]    = { "zz",     { "150"                                }                 },
-    ["zb"]    = { "zb",     { "150"                                }                 },
+    ["<C-y>"] = { "scroll", { "-0.10", "false", "80" } },
+    ["<C-e>"] = { "scroll", { "0.10", "false", "80" } },
+    ["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "150" } },
+    ["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "150" } },
+    ["<C-b>"] = { "scroll", { "-vim.api.nvim_win_get_height(0)", "true", "300" } },
+    ["<C-f>"] = { "scroll", { "vim.api.nvim_win_get_height(0)", "true", "300" } },
+    -- ["G"] = { "scroll", { "vim.api.nvim_buf_line_count(0)", "true", "1000" } },
+    -- ["gg"] = { "scroll", { "-vim.api.nvim_buf_line_count(0)", "true", "1000" } },
+    ["zt"] = { "zt", { "150" } },
+    ["zz"] = { "zz", { "150" } },
+    ["zb"] = { "zb", { "150" } },
 }
 
 return {
@@ -17,6 +17,7 @@ return {
         -- Neoscroll
         -- Smooth scrolling neovim plugin written in lua
         "karb94/neoscroll.nvim",
+        enabled = false,
         config = function()
             local loaded, neoscroll = pcall(require, "neoscroll")
             if not loaded then
@@ -26,7 +27,6 @@ return {
 
             neoscroll.setup({
                 easing_function = "quintic", -- Default easing function (`quadratic`, `cubic`, `quartic`, `quintic`, `circular`, `sine`)
-                -- stop_eof = false,
             })
 
             require("neoscroll.config").set_mappings(t)
