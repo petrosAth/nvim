392c22e0b26f038d793af591a3e0997750f17c47
petrosAth <develop@athanasoulis.me>
2023-01-17 22:22:07 +0200

    Go back on using hop.nvim instead of leap.nvim

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -1,13 +1,24 @@
+local function setup(hop)
+    hop.setup({
+        keys = "fjdkslaruvmgheiwoxqpbnz",
+    })
+end
+
 return {
     {
         -- hop
         -- Hop is an EasyMotion-like plugin allowing you to jump anywhere in a document with as few keystrokes as
         -- possible.
         "phaazon/hop.nvim",
-        enabled = false,
         branch = "v2",
         config = function()
-            require("hop").setup()
+            local loaded, hop = pcall(require, "hop")
+            if not loaded then
+                USER.loading_error_msg("hop.nvim")
+                return
+            end
+
+            setup(hop)
         end,
     },
 }
