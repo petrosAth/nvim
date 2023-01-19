3414438d645ba746e4864bb41870e797e1c9bb36
petrosAth <develop@athanasoulis.me>
2023-01-19 21:46:35 +0200

    Clean up status-column

--- lua/ui/status-column.lua
+++ lua/ui/status-column.lua
@@ -2,23 +2,6 @@
 -- roku_remote - https://www.reddit.com/r/neovim/comments/10fpqbp/comment/j4y8sd3/?utm_source=share&utm_medium=web2x&context=3
 local icons = USER.styling.icons
 
-local gitsigns_icons = {
-    GitSignsAdd = icons.git.signs.add,
-    GitSignsChange = icons.git.signs.change,
-    GitSignsChangedelete = icons.git.signs.changedelete,
-    GitSignsDelete = icons.git.signs.delete,
-    GitSignsTopdelete = icons.git.signs.topdelete,
-    GitSignsUntracked = icons.git.signs.untracked,
-}
-
-local diag_signs_icons = {
-    DiagnosticSignError = icons.lsp.error[1],
-    DiagnosticSignWarn = icons.lsp.warn[1],
-    DiagnosticSignInfo = icons.lsp.info[1],
-    DiagnosticSignHint = icons.lsp.hint[1],
-    DiagnosticSignOk = icons.lsp.ok[1],
-}
-
 local function get_sign_name(cur_sign)
     if cur_sign == nil then
         return nil
@@ -60,6 +43,14 @@ end
 
 function _G.get_statuscol_gitsign(bufnum, lnum)
     local cur_sign_nm = get_name_from_group(bufnum, lnum, "gitsigns_vimfn_signs_")
+    local gitsigns_icons = {
+        GitSignsAdd = icons.git.signs.add,
+        GitSignsChange = icons.git.signs.change,
+        GitSignsChangedelete = icons.git.signs.changedelete,
+        GitSignsDelete = icons.git.signs.delete,
+        GitSignsTopdelete = icons.git.signs.topdelete,
+        GitSignsUntracked = icons.git.signs.untracked,
+    }
 
     if cur_sign_nm ~= nil then
         return mk_hl(cur_sign_nm, gitsigns_icons[cur_sign_nm])
@@ -70,53 +61,60 @@ end
 
 function _G.get_statuscol_diag(bufnum, lnum)
     local cur_sign_nm = get_name_from_group(bufnum, lnum, "*")
+    local diag_signs_icons = {
+        DiagnosticSignError = icons.lsp.error[1],
+        DiagnosticSignWarn = icons.lsp.warn[1],
+        DiagnosticSignInfo = icons.lsp.info[1],
+        DiagnosticSignHint = icons.lsp.hint[1],
+        DiagnosticSignOk = icons.lsp.ok[1],
+    }
 
     if cur_sign_nm ~= nil and vim.startswith(cur_sign_nm, "DiagnosticSign") then
         return mk_hl(cur_sign_nm, diag_signs_icons[cur_sign_nm])
-        -- return diag_signs_icons[cur_sign_nm]
     else
         return " "
     end
 end
 
-function _G.get_statuscol_num()
-    local v = vim.v
+function _G.get_statuscol_num(lnum, relnum)
     local mode = vim.api.nvim_get_mode()["mode"]
     local winid = vim.api.nvim_get_current_win()
     local curwin = tonumber(vim.g.actual_curwin)
 
-    if mode == "i" or winid ~= curwin then
-        -- if mode == "i" then
-        return v.lnum
+    if winid ~= curwin then
+        return lnum
     end
 
-    if v.relnum == 0 then
-        return v.lnum
+    if mode == "i" then
+        return lnum
     end
 
-    return v.relnum
+    if relnum == 0 then
+        return lnum
+    end
+
+    return relnum
 end
 
-function _G.get_statuscol_fold()
-    local v = vim.v
+function _G.get_statuscol_fold(lnum)
     local fn = vim.fn
     local sign = icons.fillchars.global.foldsep
 
-    if fn.foldlevel(v.lnum) == 0 then
+    if fn.foldlevel(lnum) == 0 then
         sign = " "
     else
-        if fn.foldlevel(v.lnum) > fn.foldlevel(v.lnum - 1) then
-            if fn.foldclosed(v.lnum) == -1 then
+        if fn.foldlevel(lnum) > fn.foldlevel(lnum - 1) then
+            if fn.foldclosed(lnum) == -1 then
                 sign = icons.fillchars.global.foldopen
             else
                 sign = icons.fillchars.global.foldclose
             end
         end
 
-        if fn.foldlevel(v.lnum) > fn.foldlevel(v.lnum + 1) then
+        if fn.foldlevel(lnum) > fn.foldlevel(lnum + 1) then
             sign = icons.fillchars.extra.foldmid
 
-            if fn.foldlevel(v.lnum + 1) == 0 then
+            if fn.foldlevel(lnum + 1) == 0 then
                 sign = icons.fillchars.extra.foldend
             end
         end
@@ -130,9 +128,9 @@ _G.get_statuscol = function()
 
     local parts = {
         ["diagnostics"] = "%{%v:lua.get_statuscol_diag(bufnr(), v:lnum)%}",
-        ["fold"] = "%{v:lua.get_statuscol_fold()}",
+        ["fold"] = "%{v:lua.get_statuscol_fold(v:lnum)}",
         ["gitsigns"] = "%{%v:lua.get_statuscol_gitsign(bufnr(), v:lnum)%}",
-        ["num"] = "%{v:lua.get_statuscol_num()}",
+        ["num"] = "%{v:lua.get_statuscol_num(v:lnum, v:relnum)}",
         ["sep"] = "%=",
         ["signcol"] = "%s",
         ["space"] = " ",
@@ -140,6 +138,7 @@ _G.get_statuscol = function()
 
     local order = {
         "diagnostics",
+        -- "signcol",
         "space",
         "sep",
         "num",
