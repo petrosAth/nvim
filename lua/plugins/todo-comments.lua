7efa7fd856cfaa203f75bf98eac512e63cd321c6
petrosAth <develop@athanasoulis.me>
2023-03-03 14:38:44 +0200

    Fix todo-comments.nvim highlights

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -16,44 +16,6 @@ local function setup(todo_comments, icons)
             PERF = { icon = icons.performance[1], alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
             NOTE = { icon = icons.note[1], color = "hint", alt = { "INFO" } },
         },
-        merge_keywords = true, -- when true, custom keywords will be merged with the defaults
-        -- highlighting of the line containing the todo comment
-        -- * before: highlights before the keyword (typically comment characters)
-        -- * keyword: highlights of the keyword
-        -- * after: highlights after the keyword (todo text)
-        highlight = {
-            before = "", -- "fg" or "bg" or empty
-            keyword = "wide", -- "fg", "bg", "wide" or empty. (wide is the same as bg, but will also highlight surrounding characters)
-            after = "fg", -- "fg" or "bg" or empty
-            pattern = [[.*<(KEYWORDS)\s*:]], -- pattern or table of patterns, used for highlightng (vim regex)
-            comments_only = true, -- uses treesitter to match keywords in comments only
-            max_line_len = 400, -- ignore lines longer than this
-            exclude = {}, -- list of file types to exclude highlighting
-        },
-        -- list of named colors where we try to extract the guifg from the
-        -- list of hilight groups or use the hex color if hl not found as a fallback
-        colors = {
-            error = { "LspDiagnosticsDefaultError", "ErrorMsg", "#DC2626" }, -- FIX:
-            warning = { "LspDiagnosticsDefaultWarning", "WarningMsg", "#FBBF24" }, -- WARN:
-            info = { "LspDiagnosticsDefaultInformation", "#2563EB" }, -- TODO:
-            hint = { "LspDiagnosticsDefaultHint", "#10B981" }, -- NOTE:
-            default = { "Identifier", "#7C3AED" },
-            optimize = { "Identifier", "#7C3AED" }, -- PERF:
-        },
-        search = {
-            command = "rg",
-            args = {
-                "--color=never",
-                "--no-heading",
-                "--with-filename",
-                "--line-number",
-                "--column",
-            },
-            -- regex that will be used to match keywords.
-            -- don't replace the (KEYWORDS) placeholder
-            pattern = [[\b(KEYWORDS):]], -- ripgrep regex
-            -- pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
-        },
     })
 end
 
