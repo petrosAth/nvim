495a82ffc0625e9b88a25b5d57502cc6690f850c
petrosAth <develop@athanasoulis.me>
2023-04-10 22:48:46 +0300

    Remove default valued options from fidget.nvim config

--- lua/plugins/lsp/fidget.lua
+++ lua/plugins/lsp/fidget.lua
@@ -11,22 +11,13 @@ function M.setup(icons, borders)
         text = {
             spinner = icons.loading.sphere, -- animation shown when tasks are ongoing
             done = icons.lsp.loaded[1], -- character shown when all tasks are complete
-            commenced = "Started", -- message shown when task starts
-            completed = "Completed", -- message shown when task completes
-        },
-        align = {
-            bottom = true, -- align fidgets along bottom edge of buffer
-            right = true, -- align fidgets along right edge of buffer
         },
         timer = {
             spinner_rate = 125, -- frame rate of spinner animation, in ms
-            fidget_decay = 1000, -- how long to keep around empty fidget, in ms
-            task_decay = 500, -- how long to keep around completed task, in ms
         },
         window = {
             relative = "editor", -- where to anchor, either "win" or "editor"
             blend = USER.styling.variables.transparency, -- &winblend for the window
-            zindex = nil, -- the zindex value for the window
             border = {
                 { borders.tl, "FloatBorder" },
                 { borders.t, "FloatBorder" },
@@ -39,9 +30,6 @@ function M.setup(icons, borders)
             }, -- style of border for the fidget window
         },
         fmt = {
-            leftpad = true, -- right-justify text in fidget box
-            stack_upwards = true, -- list of tasks grows upwards
-            max_width = 0, -- maximum width of the fidget box
             -- function to format fidget title
             fidget = function(fidget_name, spinner)
                 return string.format("%s %s %s", spinner, fidget_name, icons.arrow.hollow.l)
