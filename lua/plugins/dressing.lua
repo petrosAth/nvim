434d2d96c3f4f98b5601a52203344c5430c3d0c5
petrosAth <develop@athanasoulis.me>
2023-08-05 13:14:58 +0300

    Update dressing.nvim config

--- lua/plugins/dressing.lua
+++ lua/plugins/dressing.lua
@@ -4,11 +4,7 @@ local function setup(dressing)
 
     dressing.setup({
         input = {
-            -- Set to false to disable the vim.ui.input implementation
             enabled = false,
-
-            -- These are passed to nvim_open_win
-            anchor = "SW",
             border = {
                 borders.default.tl,
                 borders.default.t,
@@ -19,32 +15,18 @@ local function setup(dressing)
                 borders.default.bl,
                 borders.default.l,
             },
-            -- 'editor' and 'win' will default to being centered
             relative = "cursor",
-
-            -- These can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
             prefer_width = 40,
             width = nil,
-            -- min_width and max_width can be a list of mixed types.
-            -- min_width = {20, 0.2} means "the greater of 20 columns or 20% of total"
             max_width = { 140, 0.9 },
             min_width = { 20, 0.2 },
-
-            -- Window transparency (0-100)
             win_options = {
                 winblend = winblend,
             },
         },
         select = {
-            -- Set to false to disable the vim.ui.select implementation
             enabled = true,
-
-            -- Priority list of preferred vim.select implementations
             backend = { "telescope", "fzf_lua", "fzf", "builtin", "nui" },
-
-            -- Options for telescope selector
-            -- These are passed into the telescope picker directly. Can be used like:
-            -- telescope = require('telescope.themes').get_ivy({...})
             telescope = require("telescope.themes").get_dropdown({
                 previewer = false,
                 results_title = false,
