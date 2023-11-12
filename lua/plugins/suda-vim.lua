9804d87e93767357e2a696c0ae7ba4561d8082ed
petrosAth <develop@athanasoulis.me>
2023-11-12 09:47:36 +0200

    Add suda.vim plugin

---
+++ lua/plugins/suda-vim.lua
@@ -0,0 +1,8 @@
+return {
+    -- suda.vim
+    -- An alternative sudo.vim for Vim and Neovim, limited support sudo in Windows
+    "lambdalisue/suda.vim",
+    init = function()
+        vim.g.suda_smart_edit = 1
+    end,
+}
