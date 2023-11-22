f1a55499e4351a8e0d62897529c891a635b6f82e
petrosAth <develop@athanasoulis.me>
2023-11-22 21:50:31 +0200

    Add markdown format in cmp documentation windows

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -16,6 +16,18 @@ local function setup(cmp)
         return vim.api.nvim_replace_termcodes(str, true, true, true)
     end
 
+    vim.lsp.util.stylize_markdown = function(bufnr, contents, opts)
+        contents = vim.lsp.util._normalize_markdown(contents, {
+            width = vim.lsp.util._make_floating_popup_size(contents, opts),
+        })
+
+        vim.bo[bufnr].filetype = "markdown"
+        vim.treesitter.start(bufnr)
+        vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, contents)
+
+        return contents
+    end
+
     cmp.setup({
         snippet = {
             expand = function(args)
