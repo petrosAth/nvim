c95f1573e84e613773f0e8d967e7a160d65ffe1e
petrosAth <develop@athanasoulis.me>
2024-10-21 22:13:02 +0300

    Update gitsigns config and add staged signs icons

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -11,31 +11,22 @@ local function setup(gitsigns)
             changedelete = { text = icons.signs.changedelete },
             untracked = { text = icons.signs.untracked },
         },
-        signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+        signs_staged = {
+            add = { text = icons.signs.add },
+            change = { text = icons.signs.change },
+            delete = { text = icons.signs.delete },
+            topdelete = { text = icons.signs.topdelete },
+            changedelete = { text = icons.signs.changedelete },
+            untracked = { text = icons.signs.untracked },
+        },
         numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
         linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
         show_deleted = false, -- Toggle with `:Gitsigns toggle_deleted`
         word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
-        watch_gitdir = {
-            interval = 1000,
-            follow_files = true,
-        },
-        attach_to_untracked = true,
-        current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
-        current_line_blame_opts = {
-            virt_text = true,
-            virt_text_pos = "eol", -- "eol" | "overlay" | "right_align"
-            delay = 0,
-            ignore_whitespace = false,
-            virt_text_priority = 0,
-        },
+        current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
         current_line_blame_formatter = "    " .. icons.commit[1] .. " <author>, <author_time:%Y-%m-%d> - <summary>",
-        sign_priority = 6,
         update_debounce = 100,
-        status_formatter = nil, -- Use default
-        max_file_length = 40000,
         preview_config = {
-            -- Options passed to nvim_open_win
             border = {
                 borders.tl,
                 borders.t,
@@ -46,10 +37,7 @@ local function setup(gitsigns)
                 borders.bl,
                 borders.l,
             },
-            style = "minimal",
-            relative = "cursor",
             row = 1,
-            col = 1,
         },
     })
 end
