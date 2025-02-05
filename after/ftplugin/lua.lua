e70770a7eec98c74203caa53a390d79beb9eee62
petrosAth <develop@athanasoulis.me>
2025-02-05 21:26:35 +0200

    Add textwidth option

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -1,6 +1,5 @@
 local ol = vim.opt_local
 
-ol.colorcolumn = "120" -- Vertical line for text alignment position
 ol.shiftwidth  = 4     -- Number of auto-indent spaces
 ol.softtabstop = 4     -- Number of spaces per Tab
 ol.tabstop     = 4     -- Number of columns per tab
