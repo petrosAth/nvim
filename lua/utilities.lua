b310c530a4ecf86897c94cbc76e7a16a474f7ca3
petrosAth <develop@athanasoulis.me>
2025-02-05 21:27:53 +0200

    Formatting

--- lua/utilities.lua
+++ lua/utilities.lua
@@ -1,8 +1,9 @@
 ---Spqwn an error notification with the argument as the message body
 ---@param plugin_name string
-function USER.loading_error_msg(plugin_name) vim.notify(string.format("%s is not installed!", plugin_name), "ERROR", { title = "Loading failed" }) end
+function USER.loading_error_msg(plugin_name)
+    vim.notify(string.format("%s is not installed!", plugin_name), "ERROR", { title = "Loading failed" })
+end
 
 ---Returns true if the provided directory is part of a git repository
----@param cwd string
 ---@return boolean
 function USER.is_git_repo() return vim.b.gitsigns_head or vim.b.gitsigns_status_dict end
