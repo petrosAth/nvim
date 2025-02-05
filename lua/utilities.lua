5620cb0705085a7b843b96857cb75b1b43154264
petrosAth <develop@athanasoulis.me>
2025-02-05 21:26:34 +0200

    Add text in global error message function

--- lua/utilities.lua
+++ lua/utilities.lua
@@ -1,6 +1,6 @@
 ---Spqwn an error notification with the argument as the message body
 ---@param plugin_name string
-function USER.loading_error_msg(plugin_name) vim.notify(plugin_name, "ERROR", { title = "Loading failed" }) end
+function USER.loading_error_msg(plugin_name) vim.notify(string.format("%s is not installed!", plugin_name), "ERROR", { title = "Loading failed" }) end
 
 ---Returns true if the provided directory is part of a git repository
 ---@param cwd string
