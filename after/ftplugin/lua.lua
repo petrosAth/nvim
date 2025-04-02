b1c49f8849ff8ca45a478198a9e6043ff17533de
petrosAth <develop@athanasoulis.me>
2025-04-02 17:52:11 +0300

    Use textwidth instead of directly setting colorcolumn
    Add .editorconfig file

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -1,5 +1,6 @@
 local ol = vim.opt_local
 
+ol.textwidth   = 120   -- Maximum width of text that is being inserted
 ol.shiftwidth  = 4     -- Number of auto-indent spaces
 ol.softtabstop = 4     -- Number of spaces per Tab
 ol.tabstop     = 4     -- Number of columns per tab
--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -1,6 +1,5 @@
 local ol = vim.opt_local
 
-ol.colorcolumn = "88" -- Vertical line for text alignment position
 ol.textwidth   = 88   -- Maximum width of text that is being inserted
 ol.shiftwidth  = 4 -- Number of auto-indent spaces
 ol.softtabstop = 4 -- Number of spaces per Tab
