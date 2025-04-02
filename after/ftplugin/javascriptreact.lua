124f0eeb2beb493776276b02b08fea54eee46156
petrosAth <develop@athanasoulis.me>
2025-04-02 17:51:57 +0300

    Change indentation width and add python ftplugin
    Change indentation width in prettier template
    
    Update prettierrc

--- after/ftplugin/javascriptreact.lua
+++ after/ftplugin/javascriptreact.lua
@@ -1,5 +1,5 @@
 local ol = vim.opt_local
 
-ol.shiftwidth  = 2 -- Number of auto-indent spaces
-ol.softtabstop = 2 -- Number of spaces per Tab
-ol.tabstop     = 2 -- Number of columns per tab
+ol.shiftwidth  = 4 -- Number of auto-indent spaces
+ol.softtabstop = 4 -- Number of spaces per Tab
+ol.tabstop     = 4 -- Number of columns per tab
--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -1,5 +1,7 @@
 local ol = vim.opt_local
 
+ol.colorcolumn = "88" -- Vertical line for text alignment position
+ol.textwidth   = 88   -- Maximum width of text that is being inserted
 ol.shiftwidth  = 4 -- Number of auto-indent spaces
 ol.softtabstop = 4 -- Number of spaces per Tab
 ol.tabstop     = 4 -- Number of columns per tab
