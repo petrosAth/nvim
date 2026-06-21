902d924a3ca129dd13054f4f0845d31955f21943
petrosAth <develop@athanasoulis.me>
2026-06-22 01:55:22 +0300

    feat(ftdetect): add custom filetype detection for common config files

---
+++ ftdetect/custom_types.lua
@@ -0,0 +1,16 @@
+vim.filetype.add({
+    extension = {
+        conf = "conf",
+        env = "dotenv",
+        tiltfile = "tiltfile",
+        Tiltfile = "tiltfile",
+    },
+    filename = {
+        [".env"] = "dotenv",
+        ["tsconfig.json"] = "jsonc",
+        [".yamlfmt"] = "yaml",
+    },
+    pattern = {
+        ["%.env%.[%w_.-]+"] = "dotenv",
+    },
+})
