d2c3a913be9e6ea03cb3373badc859ed9016189f
petrosAth <develop@athanasoulis.me>
2026-06-22 12:28:46 +0300

    feat(lsp): add lensline.nvim with focused toggle via snacks

---
+++ lua/plugins/lensline.lua
@@ -0,0 +1,41 @@
+local function setup(lensline)
+    lensline.setup({
+        profiles = {
+            {
+                name = "default",
+                providers = {
+                    {
+                        name = "usages",
+                        enabled = true,
+                        include = { "refs", "defs", "impls" },
+                        breakdown = true,
+                        show_zero = false,
+                    },
+                },
+                style = {
+                    highlight = "SpecialComment",
+                    prefix = "  ",
+                },
+            },
+        },
+    })
+end
+
+return {
+    {
+        -- A lightweight plugin that displays contextual info (LSP reference counts)
+        -- above functions, codelens style.
+        "oribarilan/lensline.nvim",
+        branch = "release/2.x",
+        event = "LspAttach",
+        config = function()
+            local loaded, lensline = pcall(require, "lensline")
+            if not loaded then
+                USER.loading_error_msg("lensline.nvim")
+                return
+            end
+
+            setup(lensline)
+        end,
+    },
+}
